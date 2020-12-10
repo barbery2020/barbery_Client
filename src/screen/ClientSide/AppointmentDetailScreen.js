f2f94b1ed91785d3e1eb403a84a4f3ad3e257b00
tuseeqraza <tuseeqraza@github.com>
2020-12-10 18:55:14 +0500

    1.2 Review in Appointments

--- src/screen/ClientSide/AppointmentDetailScreen.js
+++ src/screen/ClientSide/AppointmentDetailScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   Text,
@@ -6,8 +6,12 @@ import {
   ScrollView,
   Image,
   FlatList,
+  SafeAreaView,
   TouchableOpacity,
+  LogBox,
 } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { Rating, AirbnbRating } from 'react-native-ratings';
 import LinearGradient from 'react-native-linear-gradient';
 
 import ReviewCard from '../../components/ReviewCard';
@@ -15,12 +19,15 @@ import Separator from '../../components/Separator';
 import colors from '../../styles/colors';
 
 export default function AppointmentDetailScreen() {
-  const [isCompleted, setCompleted] = React.useState(false);
+  const [isCompleted, setCompleted] = React.useState(true);
+  const [isReviewed, setReviewed] = React.useState(false);
+  const [isReview, setReview] = React.useState('');
+  const [isRating, setRating] = React.useState(0);
 
   const customer = [
     {
       image: require('../../assets/images/image_1.jpg'),
-      name: 'Masroor Ahmad',
+      name: 'Tuseeq Raza',
       time: 'Oct 23, 2020 4:50 PM',
       package: 'nill',
       promo: 'nill',
@@ -49,12 +56,15 @@ export default function AppointmentDetailScreen() {
   const review = {
     id: 1,
     name: 'Tuseeq Raza',
-    rated: 2,
+    rated: 4,
     text:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     time: 'Oct 23, 2020 4:50 PM',
-    image: require('../../assets/images/image_1.jpg'),
   };
+
+  useEffect(() => {
+    LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
+  }, []);
 
   return (
     <ScrollView style={styles.screen}>
@@ -70,7 +80,7 @@ export default function AppointmentDetailScreen() {
           <Text style={styles.heading}>Services</Text>
           <Text style={styles.heading}>Price</Text>
         </View>
-        <View style={styles.card}>
+        <SafeAreaView style={styles.card}>
           <FlatList
             style={styles.flatScreen}
             data={services}
@@ -82,8 +92,10 @@ export default function AppointmentDetailScreen() {
               </View>
             )}
           />
+        </SafeAreaView>
+        <View style={{ paddingHorizontal: 25, paddingVertical: 10 }}>
+          <Separator />
         </View>
-        <Separator />
         <View style={styles.flatItem}>
           <Text style={styles.heading}>Promo</Text>
           <Text>-100</Text>
@@ -100,7 +112,51 @@ export default function AppointmentDetailScreen() {
           <Text style={styles.time}>specialist</Text>
         </View>
       </View>
-      {!isCompleted && (
+
+      {/* Review Card */}
+      {isCompleted && !isReviewed && (
+        <View style={styles.reviewCard}>
+          <View style={{ paddingVertical: 30 }}>
+            <Separator />
+          </View>
+          <View style={styles.inputCard}>
+            <Rating
+              style={styles.rating}
+              imageSize={30}
+              ratingCount={5}
+              startingValue={isRating}
+              onFinishRating={(rating) => setRating(rating)}
+              // readonly={true}
+            />
+            <TextInput
+              style={styles.textInput}
+              // placeholder={'e.g. abc@gmail.com'}
+              multiline
+              numberOfLines={4}
+              maxLength={200}
+              onChangeText={(text) => {
+                setReview(text);
+              }}
+              value={isReview}
+            />
+            <LinearGradient
+              colors={[colors.orange, colors.red]}
+              style={styles.button}>
+              <TouchableOpacity
+                style={{ width: '100%', alignItems: 'center' }}
+                onPress={() => {
+                  alert('Profile is updated.');
+                  // setCompleted(true);
+                  setReviewed(true);
+                }}>
+                <Text style={styles.textBtn}>Save</Text>
+              </TouchableOpacity>
+            </LinearGradient>
+          </View>
+        </View>
+      )}
+
+      {isReviewed && (
         <View style={styles.reviewCard}>
           <View style={{ paddingVertical: 30 }}>
             <Separator />
@@ -108,8 +164,8 @@ export default function AppointmentDetailScreen() {
           <ReviewCard
             title={review.name}
             time={review.time}
-            text={review.text}
-            rated={review.rated}
+            text={isReview}
+            rated={isRating}
           />
         </View>
       )}
@@ -123,8 +179,21 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
   },
   card: {
+    flex: 1,
     backgroundColor: colors.white,
     flexDirection: 'row',
+  },
+  inputCard: {
+    flex: 1,
+    // flexDirection: 'row',
+    marginHorizontal: 20,
+    marginBottom: 20,
+    overflow: 'hidden',
+    shadowRadius: 20,
+    padding: 15,
+    backgroundColor: colors.white,
+    borderRadius: 20,
+    elevation: 10,
   },
   cardPerson: {
     backgroundColor: colors.white,
@@ -136,8 +205,26 @@ const styles = StyleSheet.create({
     flex: 1,
     // marginTop: 30,
   },
+  rating: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginHorizontal: 15,
+  },
   detailsContainer: {
     marginLeft: 10,
+    marginVertical: 10,
+  },
+  textInput: {
+    height: 100,
+    fontSize: 14,
+    borderRadius: 15,
+    elevation: 1,
+    backgroundColor: colors.light,
+    padding: 10,
+    marginHorizontal: 10,
+    marginTop: 20,
+    marginBottom: 15,
+    textAlignVertical: 'top',
   },
   image: {
     width: 70,
@@ -157,7 +244,7 @@ const styles = StyleSheet.create({
   },
   flatScreen: {
     flex: 1,
-    paddingVertical: 15,
+    paddingTop: 15,
   },
   flatItem: {
     flexDirection: 'row',
@@ -189,7 +276,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 10,
     elevation: 5,
-    marginVertical: 25,
+    marginVertical: 10,
     marginHorizontal: 70,
   },
   textBtn: {
