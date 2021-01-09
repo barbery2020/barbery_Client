c10430df4564052b49ca1d014faf9b970fe8d3ca
tuseeqraza <69717044+tuseeqraza@users.noreply.github.com>
2021-01-09 18:24:42 +0500

    Dev Merge (#1)
    * Delete LICENSE
    
    * 1.0 Auth screens
    
    Basic Foilder structure, Auth screen and gradle setting
    
    * 1.0 Profile screen added
    
    * 1.1 Appointments screens
    
    * 1.2 Review in Appointments
    
    * 1.3 BottomTabs
    
    * 1.4 Home screen
    
    * 1.5 Home screen CARDS
    
    * 1.6 Salon Screen
    
    * 1.7 Salon Reviews
    
    * 1.8 Services added
    
    * 1.9 Services Selection for Appointment
    
    * 2.0 Specialist details and Google Maps
    
    * 2.1 Gallery
    
    * 2.2 Forget Screen and Photos API
    
    * 2.3 Map Screen
    
    * 2.4 Time and barber Selection for appointment
    
    * 3.0 Integration basic settings and welcome
    
    * 3.1 Integration sign in/up/out
    
    * Tauseeq Changes
    
    * 3.2 Integration appointment
    
    * 3.3 Integration fixes
    
    * 3.4 Integration appointments view
    
    * 3.5 Integration appointments
    
    * 3.6 Integration
    
    * 3.6 Integration
    
    * 3.7 Integration
    
    * 3.8 Integration
    
    * 3.9 Integration
    
    Co-authored-by: tuseeqraza <tuseeqraza@github.com>

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {Text, View} from 'react-native';
 
-export default function App() {
+import ClientStack from './src/components/AuthStack';
+import axios from 'axios';
+import setAuthToken from './config';
+import { connect } from 'react-redux';
+import { StatusBar } from 'react-native';
+import colors from './src/styles/colors';
+
+function App({ token }) {
   return (
-    <View>
-      <Text>Hello Barbery Users</Text>
-    </View>
+    <>
+      <StatusBar backgroundColor={colors.red} />
+      <ClientStack />
+    </>
   );
 }
+
+const mapStateToProps = ({ user: { token } }) => ({ token });
+
+export default connect(mapStateToProps)(App);
